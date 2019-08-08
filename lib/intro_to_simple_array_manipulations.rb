26000251571c9488bdb333bed22c592cf0063cdd
Lizzy Tong <lizzy.tong33@gmail.com>
2019-08-08 21:52:04 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,31 @@
+def using_push(array, new_element)
+  array.push(new_element)
+  end
+
+def using_unshift (array, new_element)
+  array.unshift(new_element)
+end
+
+def using_pop (array)
+  array.pop
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array)
+  array.shift
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
+
+def using_concat(array1, array2)
+  array1.concat(array2)
+end
+
+def using_insert(array, new_element)
+  array.insert(4, new_element)
+end
